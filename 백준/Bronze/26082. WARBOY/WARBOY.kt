4163f7131f4dc46bc75a63accc65627191bccd66
Janghyun Roh <113655323+janghyunroh@users.noreply.github.com>
2025-02-11 21:52:47 +0900

    [Bronze V] Title: WARBOY, Time: 100 ms, Memory: 13256 KB -BaekjoonHub

---
+++ 백준/Bronze/26082. WARBOY/WARBOY.kt
@@ -0,0 +1,11 @@
+import java.util.*
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    val A = scanner.nextInt()
+    val B = scanner.nextInt()
+    val C = scanner.nextInt()
+    
+    val warboyPerformance = (B / A) * C * 3
+    println(warboyPerformance)
+}
