423028debfc7ee78a800728f39ffb8290fee3616
Janghyun Roh <113655323+janghyunroh@users.noreply.github.com>
2025-03-05 22:09:11 +0900

    [Bronze III] Title: 얼마?, Time: 72 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/9325. 얼마？/얼마？.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+
+for _ in range(T):
+    sum = 0
+    s = int(input())
+    sum += s
+    
+    n = int(input())
+    for _ in range(n):
+        q, p = map(int, input().split())
+        sum += q * p
+        
+    print(sum)
+        
+    
